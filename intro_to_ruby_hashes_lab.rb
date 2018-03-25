f686f8d868029ba43ee884fd5fc6b5b44fde2430
Vanessa Toro <vanetoro84@gmail.com>
2018-03-25 17:16:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,8 @@ def monopoly_with_third_tier
 	  :railroad => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {}
+	      :reading_railroad => {},
+	      :pennsylvainia_railroad => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
